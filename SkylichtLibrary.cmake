031609ce584bcff5a95d7b4d60c8f812e3f415d8
ducphamhong <hongduc.pr@gmail.com>
2024-04-04 09:45:48 +0700

    Update build on Linux

--- SkylichtLibrary.cmake
+++ SkylichtLibrary.cmake
@@ -3,17 +3,17 @@
 set(SKYLICHT_TARGETS_DIR "./InstallLibs/cmake")
 
 option(USE_SKYLICHT_SHARED_LIBS "Use skylicht shared libraries" OFF)
-option(USE_BULLET_PHYSIC_ENGINE "Use bullet physics engine" ON)
 option(USE_SKYLICHT_NETWORK "Use skylicht network libraries" ON)
-
-# bullet engine
-if (USE_BULLET_PHYSIC_ENGINE)
-	add_definitions(-DUSE_BULLET_PHYSIC_ENGINE)
-endif()
+option(USE_SKYLICHT_IMGUI "Use imgui libraries" ON)
+option(USE_BULLET_PHYSIC_ENGINE "Use bullet physics engine" ON)
 
 if (NOT USE_SKYLICHT_SHARED_LIBS)
 	add_definitions(-D_IRR_STATIC_LIB_)
 	add_definitions(-D_SKYLICHT_STATIC_LIB_)
+endif()
+
+if (USE_BULLET_PHYSIC_ENGINE)
+	add_definitions(-DUSE_BULLET_PHYSIC_ENGINE)
 endif()
 
 include(${SKYLICHT_TARGETS_DIR}/ThirdPartyTargets.cmake)
@@ -43,4 +43,7 @@ endif()
 include(${SKYLICHT_TARGETS_DIR}/LightmapperTargets.cmake)
 include(${SKYLICHT_TARGETS_DIR}/UserInterfaceTargets.cmake)
 include(${SKYLICHT_TARGETS_DIR}/ClientTargets.cmake)
+
+if (USE_SKYLICHT_IMGUI)
 include(${SKYLICHT_TARGETS_DIR}/ImguiTargets.cmake)
+endif()
