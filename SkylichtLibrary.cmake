8f18cc2a0dd1abf80b7e538c86ae65b3b9478d2c
ducphamhong <hongduc.pr@gmail.com>
2024-04-04 09:25:59 +0700

    Update workflows/build.yml

--- SkylichtLibrary.cmake
+++ SkylichtLibrary.cmake
@@ -1,4 +1,21 @@
-set(SKYLICHT_TARGETS_DIR "${HELLO_SKYLICHT_SOURCE_DIR}/InstallLibs/cmake")
+# Target of install library from https://github.com/skylicht-lab/skylicht-engine
+# skylicht-engine\BuildCommand\InstallLibVCPrj2022.cmd
+set(SKYLICHT_TARGETS_DIR "./InstallLibs/cmake")
+
+option(USE_SKYLICHT_SHARED_LIBS "Use skylicht shared libraries" OFF)
+option(USE_BULLET_PHYSIC_ENGINE "Use bullet physics engine" ON)
+option(USE_SKYLICHT_NETWORK "Use skylicht network libraries" ON)
+
+# bullet engine
+if (USE_BULLET_PHYSIC_ENGINE)
+	add_definitions(-DUSE_BULLET_PHYSIC_ENGINE)
+endif()
+
+if (NOT USE_SKYLICHT_SHARED_LIBS)
+	add_definitions(-D_IRR_STATIC_LIB_)
+	add_definitions(-D_SKYLICHT_STATIC_LIB_)
+endif()
+
 include(${SKYLICHT_TARGETS_DIR}/ThirdPartyTargets.cmake)
 include(${SKYLICHT_TARGETS_DIR}/SystemTargets.cmake)
 include(${SKYLICHT_TARGETS_DIR}/AudioTargets.cmake)
@@ -6,15 +23,23 @@ include(${SKYLICHT_TARGETS_DIR}/IrrlichtTargets.cmake)
 include(${SKYLICHT_TARGETS_DIR}/EngineTargets.cmake)
 include(${SKYLICHT_TARGETS_DIR}/ComponentsTargets.cmake)
 include(${SKYLICHT_TARGETS_DIR}/CollisionTargets.cmake)
+
+if (USE_BULLET_PHYSIC_ENGINE)
 include(${SKYLICHT_TARGETS_DIR}/LinearMathTargets.cmake)
 include(${SKYLICHT_TARGETS_DIR}/Bullet3CommonTargets.cmake)
 include(${SKYLICHT_TARGETS_DIR}/BulletCollisionTargets.cmake)
 include(${SKYLICHT_TARGETS_DIR}/BulletDynamicsTargets.cmake)
 include(${SKYLICHT_TARGETS_DIR}/BulletInverseDynamicsTargets.cmake)
 include(${SKYLICHT_TARGETS_DIR}/BulletSoftBodyTargets.cmake)
+endif()
+
 include(${SKYLICHT_TARGETS_DIR}/PhysicsTargets.cmake)
 include(${SKYLICHT_TARGETS_DIR}/CryptoLibTargets.cmake)
+
+if (USE_SKYLICHT_NETWORK)
 include(${SKYLICHT_TARGETS_DIR}/NetworkTargets.cmake)
+endif()
+
 include(${SKYLICHT_TARGETS_DIR}/LightmapperTargets.cmake)
 include(${SKYLICHT_TARGETS_DIR}/UserInterfaceTargets.cmake)
 include(${SKYLICHT_TARGETS_DIR}/ClientTargets.cmake)
