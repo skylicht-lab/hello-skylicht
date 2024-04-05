da38b911ab9492fa093473b539c2ae222a452ce7
ducphamhong <hongduc.pr@gmail.com>
2024-04-05 09:28:31 +0700

    Update build.yml and README.md

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
 # hello-skylicht
+
+This is an example project for integrating [Skylicht Engine](https://github.com/skylicht-lab/skylicht-engine) into another project.
+
+[![Build status](https://github.com/skylicht-lab/hello-skylicht/workflows/build/badge.svg)](https://github.com/skylicht-lab/hello-skylicht/actions?workflow=build)
+
+## How build
+
+### Step 1
+You need to clone and build Skylich Engine first.
+```Shell
+C:\>git clone https://github.com/skylicht-lab/skylicht-engine.git
+```
+
+And build the engine
+
+```Shell
+C:\skylicht-engine>cmake -S . -B ./PrjVisualStudio -G "Visual Studio 17 2022" -A x64 -DINSTALL_LIBS=ON
+C:\skylicht-engine>cmake --build ./PrjVisualStudio --target install --config Release
+```
+
+### Step 2
+
+Clone this project
+
+```Shell
+C:\>git clone https://github.com/skylicht-lab/hello-skylicht.git
+```
+
+Copy 2 folders (cmake and include) from folder build result C:\skylicht-engine\InstallLibs to this project
+
+```Shell
+C:\hello-skylicht>md InstallLibs
+C:\hello-skylicht>xcopy C:\skylicht-engine\InstallLibs\cmake InstallLibs /E
+C:\hello-skylicht>xcopy C:\skylicht-engine\InstallLibs\include InstallLibs /E
+```
+### Step 3
+
+Build project
+
+```Shell
+C:\hello-skylicht>cmake -S . -B ./PrjVisualStudio -G "Visual Studio 17 2022" -A x64
+C:\hello-skylicht>cmake --build ./PrjVisualStudio --config %BUILD_TYPE%
+```
+
+You can see the Github workflow for more details
--- SkylichtLibrary.cmake
+++ SkylichtLibrary.cmake
@@ -6,6 +6,9 @@ option(USE_SKYLICHT_SHARED_LIBS "Use skylicht shared libraries" OFF)
 option(USE_SKYLICHT_NETWORK "Use skylicht network libraries" ON)
 option(USE_SKYLICHT_IMGUI "Use imgui libraries" ON)
 option(USE_BULLET_PHYSIC_ENGINE "Use bullet physics engine" ON)
+option(USE_OPENMP "Use openmp for multithread optimize" ON)
+
+include(PlatformConfig.cmake)
 
 if (NOT USE_SKYLICHT_SHARED_LIBS)
 	add_definitions(-D_IRR_STATIC_LIB_)
@@ -15,6 +18,51 @@ endif()
 if (USE_BULLET_PHYSIC_ENGINE)
 	add_definitions(-DUSE_BULLET_PHYSIC_ENGINE)
 endif()
+
+if (USE_OPENMP)
+	if (CMAKE_CXX_COMPILER_ID STREQUAL "AppleClang")
+		# AppleClang
+		find_package(OpenMP REQUIRED)
+		if (OPENMP_FOUND)
+			set(OpenMP_C_FLAGS "-Xpreprocessor -fopenmp")
+			set(OpenMP_CXX_FLAGS "-Xpreprocessor -fopenmp")
+			
+			set (CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${OpenMP_C_FLAGS}")
+			set (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${OpenMP_CXX_FLAGS}")
+			
+			find_file(OMP_H_PATH NAME omp.h HINTS OpenMP)
+			get_filename_component(OMP_INCLUDE_DIR ${OMP_H_PATH} DIRECTORY)
+			include_directories(${OMP_INCLUDE_DIR})
+			
+			find_library(OMP_LIBRARY NAMES omp)
+			link_libraries(${OMP_LIBRARY})
+			
+			message(STATUS "- OpenMP library found: ${OMP_H_PATH}")
+			message(STATUS "- OpenMP include directory: ${OMP_INCLUDE_DIR}")
+			message(STATUS "- OpenMP link directory: ${OMP_LIBRARY}")
+			
+			add_definitions(-DUSE_OPENMP)
+		else()
+			message(STATUS "! OpenMP library not found! $>brew install libomp")
+		endif()
+	elseif (CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
+		# using Visual Studio C++
+		add_compile_options(/openmp)
+		add_definitions(-DUSE_OPENMP)
+	else()
+		# using GCC & Clang++ (android)
+		set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fopenmp")
+		set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -fopenmp")
+		add_definitions(-DUSE_OPENMP)
+	endif()
+endif()
+
+if (USE_SKYLICHT_NETWORK)
+	add_definitions(-DUSE_SKYLICHT_NETWORK)
+endif()
+
+# freetype font library
+add_definitions(-DFT2_BUILD_LIBRARY)
 
 include(${SKYLICHT_TARGETS_DIR}/ThirdPartyTargets.cmake)
 include(${SKYLICHT_TARGETS_DIR}/SystemTargets.cmake)
